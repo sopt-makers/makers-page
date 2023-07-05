ed64b69c85b946de86e3135bee1118e0317f85fb
Tekiter <tekiter8@gmail.com>
2023-07-05 22:48:11 +0900

    feat: Initial Commit

---
+++ apps/makers-page/src/app/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export const runtime = "edge";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ["next", "turbo", "prettier"],
+  plugins: ["simple-import-sort"],
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
+  },
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "@next/next/no-img-element": "off",
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "warn",
+  },
+};
