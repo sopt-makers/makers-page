8792b972a1c3341d782a24059e2ed558224bd3b6
Tekiter <tekiter8@gmail.com>
2023-07-08 23:30:58 +0900

    feat: env 테스트 수정

--- apps/web/src/env/server.ts
+++ apps/web/src/env/server.ts
@@ -1,11 +1,11 @@
 import 'server-only';
 
 export const SSR_GATEWAY_URL = process.env.SSR_GATEWAY_URL;
-export const SSR_GATEWAY_API_KEY = required('SSR_GATEWAY_API_KEY', process.env.SSR_GATEWAY_API_KEY);
+export const SSR_GATEWAY_API_KEY = process.env.SSR_GATEWAY_API_KEY;
 
-console.log('Env Const', process.env.SSR_GATEWAY_URL);
+console.log('Env Const', process.env.SSR_GATEWAY_API_KEY);
 
-function required(key: string, value: unknown) {
+function _required(key: string, value: unknown) {
   if (typeof value !== 'string' || value === '') {
     throw new Error(`Env value ${key} is not properly set.`);
   }
