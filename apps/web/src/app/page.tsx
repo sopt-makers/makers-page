9f995114cc41c132fad26f65ec5ff9696fb93d0e
Tekiter <tekiter8@gmail.com>
2023-07-08 22:57:20 +0900

    feat: 환경변수 테스터 추가

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
 import { internalGateway } from '../gateway/internal';
 
 export default async function Page() {
-  console.log('Env:', process.env);
+  console.log('Env Page:', process.env.SSR_GATEWAY_URL);
   const result = await internalGateway.ping.query('hello');
 
   return <div className='flex text-center'>Hello world! {result}</div>;
--- apps/web/src/env/server.ts
+++ apps/web/src/env/server.ts
@@ -3,6 +3,8 @@ import 'server-only';
 export const SSR_GATEWAY_URL = process.env.SSR_GATEWAY_URL;
 export const SSR_GATEWAY_API_KEY = required('SSR_GATEWAY_API_KEY', process.env.SSR_GATEWAY_API_KEY);
 
+console.log('Env Const', process.env.SSR_GATEWAY_URL);
+
 function required(key: string, value: unknown) {
   if (typeof value !== 'string' || value === '') {
     throw new Error(`Env value ${key} is not properly set.`);
