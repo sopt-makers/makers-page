d1531c2b349310bfd4674a240b866998d6d9d9d0
Tekiter <tekiter8@gmail.com>
2023-07-08 22:27:28 +0900

    feat: env 테스트 추가

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { internalGateway } from '../gateway/internal';
 
 export default async function Page() {
+  console.log('Env:', process.env);
   const result = await internalGateway.ping.query('hello');
 
   return <div className='flex text-center'>Hello world! {result}</div>;
