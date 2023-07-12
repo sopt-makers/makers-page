7edc04039fa3a9c128564fda0c053ae1a157fff8
Tekiter <tekiter8@gmail.com>
2023-07-12 17:45:46 +0900

    feat: 에러 페이지 추가

--- apps/web/src/app/recruit/error.tsx
+++ apps/web/src/app/recruit/error.tsx
@@ -1,15 +1,17 @@
 'use client';
 
-import { useEffect } from 'react';
+import { FC, useEffect } from 'react';
 
-export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+interface ErrorPageProps {}
+
+const ErrorPage: FC<ErrorPageProps> = ({ error, reset }: { error: Error; reset: () => void }) => {
   useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
-    <div className='text-center'>
-      <h2>알 수 없는 오류가 발생했습니다.</h2>
+    <div>
+      <h2>Notion 데이터를 불러오는데 실패했습니다.</h2>
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
@@ -20,4 +22,6 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
       </button>
     </div>
   );
-}
+};
+
+export default ErrorPage;
