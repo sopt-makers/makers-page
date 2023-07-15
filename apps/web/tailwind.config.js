ac425749b041cad7311d5f87abf75161d0fff133
Tekiter <tekiter8@gmail.com>
2023-07-16 02:41:51 +0900

    feat: 폰트 적용

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,7 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+
   theme: {
+    colors: {
+      black100: '#0F1010',
+    },
+    fontFamily: {
+      sans: ['var(--font-suit)', ...defaultTheme.fontFamily.sans],
+      mono: [...defaultTheme.fontFamily.mono],
+    },
     extend: {},
   },
   plugins: [],
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -23,4 +23,12 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
 };
