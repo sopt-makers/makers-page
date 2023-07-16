3abbbf41559204a01f3b8ca88432ff1baa5dfeb2
Brokyeom <goindol40@gmail.com>
2023-07-16 15:29:05 +0900

    feat : greeting 추가

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,16 @@
+import Header from '@/components/header/Header';
+import Greeting from '@/components/mainpage/Greeting';
+
 import { gateway } from '../gateway';
 
 export default async function Page() {
   const result = await gateway.internal.ping.query('hello');
 
-  return <div className='flex text-center'>Hello world! {result}</div>;
+  return (
+    <>
+      <Header />
+      <Greeting />
+      <div className='flex text-center'>Hello world! {result}</div>
+    </>
+  );
 }
---
+++ apps/web/src/components/mainpage/Greeting.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Greeting = () => {
+  return (
+    <section className='flex-col'>
+      <p>
+        솝트를 위해,
+        <br />
+        그리고 우리를 위해,
+      </p>
+      <p>
+        또 스스로를 위해 <br />
+        끊임 없는 성장을 추구합니다.
+      </p>
+    </section>
+  );
+};
+
+export default Greeting;
