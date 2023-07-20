c4bdebea31850f21e3ffdf7b2f77b3e7f9ef461e
Brokyeom <goindol40@gmail.com>
2023-07-20 18:12:54 +0900

    chore : color setting

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
     colors: {
       black100: '#0F1010',
       white: '#FFFFFF',
+      main_makers: '#FF6E1D',
+      sub_skyblue: '#5DDBFF',
+      sub_blue: '#1F299C',
+      sub_pink: '#FDBBF9',
+      sub_yellow: '#FFCA00',
     },
     fontFamily: {
       sans: ['var(--font-suit)', ...defaultTheme.fontFamily.sans],
