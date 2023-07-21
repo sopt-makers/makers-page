3bb6a03f980c886aa798c674ae5fcf5d544ef9e0
Tekiter <tekiter8@gmail.com>
2023-07-21 17:30:59 +0900

    feat: 폰트와 테마 적용 (#4)
    * feat: 폰트 적용
    
    * feat: 흰색 텍스트 적용
    
    * feat: color scheme 적용

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,13 +1,42 @@
 import './global.css';
 
+import clsx from 'clsx';
+import localFont from 'next/font/local';
 import { ReactNode } from 'react';
 
 export const runtime = 'edge';
 
+const suitFont = localFont({
+  src: [
+    {
+      path: './SUIT-Light.woff2',
+      weight: '300',
+    },
+    {
+      path: './SUIT-Regular.woff2',
+      weight: '400',
+    },
+    {
+      path: './SUIT-Medium.woff2',
+      weight: '500',
+    },
+    {
+      path: './SUIT-SemiBold.woff2',
+      weight: '600',
+    },
+    {
+      path: './SUIT-Bold.woff2',
+      weight: '700',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-suit',
+});
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='ko'>
-      <body>{children}</body>
+    <html lang='ko' className={(clsx(suitFont.variable), 'bg-black100 text-white')}>
+      <body className='min-h-screen'>{children}</body>
     </html>
   );
 }
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,7 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+
   theme: {
+    colors: {
+      black100: '#0F1010',
+      white: '#FFFFFF',
+    },
+    fontFamily: {
+      sans: ['var(--font-suit)', ...defaultTheme.fontFamily.sans],
+      mono: [...defaultTheme.fontFamily.mono],
+    },
     fontSize: {
       h0: [
         '70px',
