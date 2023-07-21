4c9edb6900321bf8d04984251182a60316371732
Brokyeom <goindol40@gmail.com>
2023-07-21 17:58:44 +0900

    fix : syntax error

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -18,34 +18,34 @@ module.exports = {
     fontFamily: {
       sans: ['var(--font-suit)', ...defaultTheme.fontFamily.sans],
       mono: [...defaultTheme.fontFamily.mono],
-      fontSize: {
-        h0: [
-          '70px',
-          {
-            lineHeight: '140%',
-            letterSpacing: '-0.7px',
-            fontWeight: '700',
-          },
-        ],
-        h1: [
-          '50px',
-          {
-            lineHeight: '100%',
-            letterSpacing: '-0.5px',
-            fontWeight: 700,
-          },
-        ],
-        h2: [
-          '30px',
-          {
-            lineHeight: '170%',
-            letterSpacing: '-0.3px',
-            fontWeight: 600,
-          },
-        ],
-      },
-      extend: {},
     },
-    plugins: [],
+    fontSize: {
+      h0: [
+        '70px',
+        {
+          lineHeight: '140%',
+          letterSpacing: '-0.7px',
+          fontWeight: '700',
+        },
+      ],
+      h1: [
+        '50px',
+        {
+          lineHeight: '100%',
+          letterSpacing: '-0.5px',
+          fontWeight: 700,
+        },
+      ],
+      h2: [
+        '30px',
+        {
+          lineHeight: '170%',
+          letterSpacing: '-0.3px',
+          fontWeight: 600,
+        },
+      ],
+    },
+    extend: {},
   },
+  plugins: [],
 };
