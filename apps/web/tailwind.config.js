e45e9439a7d3c219261af569ea67f17924b2c111
Brokyeom <goindol40@gmail.com>
2023-07-22 05:38:51 +0900

    code review : colors 속성 kebab-case 로 변경

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -6,14 +6,14 @@ module.exports = {
 
   theme: {
     colors: {
-      black100: '#0F1010',
-      white: '#FFFFFF',
-      main_makers: '#FF6E1D',
-      sub_skyblue: '#5DDBFF',
-      sub_blue: '#1F299C',
-      sub_pink: '#FDBBF9',
-      sub_yellow: '#FFCA00',
-      dark1: '#252629',
+      'black100': '#0F1010',
+      'white': '#FFFFFF',
+      'main-makers': '#FF6E1D',
+      'sub-skyblue': '#5DDBFF',
+      'sub-blue': '#1F299C',
+      'sub-pink': '#FDBBF9',
+      'sub-yellow': '#FFCA00',
+      'dark1': '#252629',
     },
     fontFamily: {
       sans: ['var(--font-suit)', ...defaultTheme.fontFamily.sans],
