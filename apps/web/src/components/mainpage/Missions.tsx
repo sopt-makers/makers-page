a103e6035a584961f7d772801ef93945396fb7e6
Brokyeom <goindol40@gmail.com>
2023-07-23 00:31:33 +0900

    feat : missions  추가

---
+++ apps/web/src/components/mainpage/Missions.tsx
@@ -0,0 +1,22 @@
+export default function Missions() {
+  return (
+    <article className='flex flex-col justify-center items-center gap-[1rem] mt-[19.69rem]'>
+      <p className='flex gap-[1.12rem]'>
+        <span className='text-sub-skyblue'>MISSION 1.</span>
+        <span className='text-h1'>
+          3천명이 넘는 SOPT 구성원들의
+          <br />
+          연결되지 못한 가치를 발견하고,
+        </span>
+      </p>
+      <p className='flex gap-[1.12rem]'>
+        <span className='text-main-makers'>MISSION 2.</span>
+        <span className='text-h1'>SOPT 활동이 더 즐거울 수 있도록 도우며,</span>
+      </p>
+      <p className='flex gap-[1.12rem]'>
+        <span className='text-sub-pink'>MISSION 3.</span>
+        <span className='text-h1'>SOPT를 대내외적으로 더 잘 알려요.</span>
+      </p>
+    </article>
+  );
+}
