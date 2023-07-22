ab3f05955fa1fb573efcbb04a1903209001b54bd
Tekiter <tekiter8@gmail.com>
2023-07-23 00:46:22 +0900

    feat: strict 옵션 추가

--- apps/web/src/components/core/animation/AnimationProviders.tsx
+++ apps/web/src/components/core/animation/AnimationProviders.tsx
@@ -8,7 +8,11 @@ interface ProvidersProps {
 
 const AnimationProviders: FC<ProvidersProps> = ({ children }) => {
   // Lazy Load를 여기서 직접 적용하진 않지만, 유사시를 대비해 미리 LazyMotion 감싸놓기
-  return <LazyMotion features={domMax}>{children}</LazyMotion>;
+  return (
+    <LazyMotion features={domMax} strict>
+      {children}
+    </LazyMotion>
+  );
 };
 
 export default AnimationProviders;
