c66d9850483c79533f591ebee4b646455f42a8ea
Brokyeom <goindol40@gmail.com>
2023-07-23 15:08:33 +0900

    chore : dummy_logo 추가

--- apps/web/src/components/mainpage/Greeting.tsx
+++ apps/web/src/components/mainpage/Greeting.tsx
@@ -1,6 +1,10 @@
 export default function greeting() {
   return (
     <article className='flex flex-col justify-center items-center font-sans'>
+      <img
+        src='https://github.com/Brokyeom/modeling_final/assets/97586683/01d5d046-27fb-4812-9a3e-4f11cb9bbf08'
+        alt='dummy_logo'
+      />
       <h1 className='mt-[3.75rem] text-center text-h0'>
         SOPT에 없던 새로운 가치를 <br />
         제품을 통해 만들어갑니다.
