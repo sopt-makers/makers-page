97cfbbde7f14bea7aecc5c127ea2a6b096a2587b
Brokyeom <goindol40@gmail.com>
2023-07-26 00:39:11 +0900

    feat : 문구별 애니메이션 추가

--- apps/web/src/components/mainpage/greeting/Missions.tsx
+++ apps/web/src/components/mainpage/greeting/Missions.tsx
@@ -13,31 +13,30 @@ export default function Missions({ className }: GreetingProps) {
 
   const { scrollYProgress } = useScroll({ target: containerRef, offset: ['start start', 'end start'] });
 
-  const opacity = useTransform(scrollYProgress, [0, 0.12, 0.3, 0.5, 1], [0, 1, 1, 0, 0]);
+  const opacity1 = useTransform(scrollYProgress, [0, 0.1, 0.7, 0.75, 1], [0, 1, 1, 0, 0]);
+  const opacity2 = useTransform(scrollYProgress, [0.14, 0.4, 0.7, 0.75, 1], [0, 1, 1, 0, 0]);
+  const opacity3 = useTransform(scrollYProgress, [0.44, 0.6, 0.7, 0.75, 1], [0, 1, 1, 0, 0]);
 
   return (
-    <article ref={containerRef} className={clsx('h-[200vh]', className)}>
-      <m.div
-        className='sticky top-0 pt-[27.2rem] flex flex-col justify-center items-center gap-[1.6rem] text-60-semibold'
-        style={{ opacity }}
-      >
-        <p className='flex justify-center items-start gap-[1.792rem]'>
+    <article ref={containerRef} className={clsx('h-[340vh]', className)}>
+      <div className='sticky top-0 pt-[27.2rem] flex flex-col justify-center items-center gap-[1.6rem] text-60-semibold'>
+        <m.p className='flex justify-center items-start gap-[1.792rem]' style={{ opacity: opacity1 }}>
           <span className='text-sub-skyblue text-16-regular'>MISSION 1.</span>
           <span>
             3천명이 넘는 SOPT 구성원들의
             <br />
             연결되지 못한 가치를 발견하고,
           </span>
-        </p>
-        <p className='flex gap-[1.792rem]'>
+        </m.p>
+        <m.p className='flex gap-[1.792rem]' style={{ opacity: opacity2 }}>
           <span className='text-main-makers text-16-regular'>MISSION 2.</span>
           <span>SOPT 활동이 더 즐거울 수 있도록 도우며,</span>
-        </p>
-        <p className='flex gap-[1.792rem]'>
+        </m.p>
+        <m.p className='flex gap-[1.792rem]' style={{ opacity: opacity3 }}>
           <span className='text-sub-pink text-16-regular'>MISSION 3.</span>
           <span>SOPT를 대내외적으로 더 잘 알려요.</span>
-        </p>
-      </m.div>
+        </m.p>
+      </div>
     </article>
   );
 }
