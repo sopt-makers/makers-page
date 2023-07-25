4bddddc0318430f96ff6ff6bde962f8083157210
Tekiter <tekiter8@gmail.com>
2023-07-25 22:23:21 +0900

    feat: 카드 플립 구현

--- apps/web/src/components/mainpage/coreValue/CoreValueSection.tsx
+++ apps/web/src/components/mainpage/coreValue/CoreValueSection.tsx
@@ -1,12 +1,10 @@
 import clsx from 'clsx';
 import { useMotionValueEvent, useScroll, useTransform } from 'framer-motion';
 import { m } from 'framer-motion';
-import { Orbitron } from 'next/font/google';
-import { FC, useRef } from 'react';
+import { FC, ReactNode, useRef } from 'react';
 
 import { BaseImage, ConnectionImage, OpportunityImage, OwnershipImage, PleasureImage } from './images';
-
-const orbitron = Orbitron({ subsets: ['latin'], preload: false, display: 'swap' });
+import ValueCard from './ValueCard';
 
 interface CoreValueSectionProps {}
 
@@ -26,19 +24,8 @@ const CoreValueSection: FC<CoreValueSectionProps> = ({}) => {
             <span>CORE</span>
             <span>VALUE</span>
           </div>
-          {cards.map((card) => (
-            <div
-              key={card.keyword}
-              className='bg-gray2 flex h-[52rem] w-[36.8rem] flex-col overflow-clip rounded-[0.8rem]'
-            >
-              <div className=''>{card.image}</div>
-              <div className='flex flex-1 flex-col items-center justify-center'>
-                <span className={clsx(orbitron.className, 'text-[3.6rem] font-bold leading-[100%]')}>
-                  {card.keyword}
-                </span>
-                <span className='mt-[2rem] text-[2.8rem] font-semibold'>{card.name}</span>
-              </div>
-            </div>
+          {cards.map((card, idx) => (
+            <ValueCard key={card.keyword} className='h-[52rem] w-[36.8rem]' {...card} seq={idx + 1} />
           ))}
         </div>
       </m.div>
@@ -46,12 +33,83 @@ const CoreValueSection: FC<CoreValueSectionProps> = ({}) => {
   );
 };
 
+interface CardBaseProps {
+  className?: string;
+  children?: ReactNode;
+}
+
+const DescriptionCard: FC<CardBaseProps> = ({ className, children }) => {
+  return (
+    <div className={clsx('flex h-full w-full items-center justify-center whitespace-pre-line text-center', className)}>
+      {children}
+    </div>
+  );
+};
+
 const cards = [
-  { keyword: 'Ownership', name: '오너십', image: <OwnershipImage /> },
-  { keyword: 'Connection', name: '연결', image: <ConnectionImage /> },
-  { keyword: 'Base', name: '기반', image: <BaseImage /> },
-  { keyword: 'Opportunity', name: '기회', image: <OpportunityImage /> },
-  { keyword: 'Pleasure', name: '즐거움', image: <PleasureImage /> },
+  {
+    keyword: 'Ownership',
+    name: '오너십',
+    image: <OwnershipImage />,
+    description: (
+      <DescriptionCard className='bg-brand-orange text-orange-sub'>
+        {`주체적인 책임감으로
+          스스로 필요한 가치를 발견하고,
+          모두가 앞으로 나아가는
+          추진력을 경험합니다.`}
+      </DescriptionCard>
+    ),
+  },
+  {
+    keyword: 'Connection',
+    name: '연결',
+    image: <ConnectionImage />,
+    description: (
+      <DescriptionCard className='bg-brand-skyblue text-skyblue-sub'>
+        {`자유로운 네트워킹 환경 속
+          뛰어난 동료와 깊이 연결되는
+          경험을 할 수 있습니다.`}
+      </DescriptionCard>
+    ),
+  },
+  {
+    keyword: 'Base',
+    name: '기반',
+    image: <BaseImage />,
+    description: (
+      <DescriptionCard className='bg-brand-blue text-blue-sub'>
+        {`조직의 단단한 기반으로써,
+          성장에 필요한 믿음직한 발판을
+          제공합니다.`}
+      </DescriptionCard>
+    ),
+  },
+  {
+    keyword: 'Opportunity',
+    name: '기회',
+    image: <OpportunityImage />,
+    description: (
+      <DescriptionCard className='bg-brand-pink text-pink-sub'>
+        {`지속적인 경험을 통해
+          시야를 확장하고,
+          더 넓고 새로운 경험의
+          기회를 선사합니다.`}
+      </DescriptionCard>
+    ),
+  },
+  {
+    keyword: 'Pleasure',
+    name: '즐거움',
+    image: <PleasureImage />,
+    description: (
+      <DescriptionCard className='bg-brand-yellow text-yellow-sub'>
+        {`존중을 기반으로,
+          진정으로 모두가 즐겁게
+          스스로의 목표를 세우고
+          이루는 경험을 선사합니다.`}
+      </DescriptionCard>
+    ),
+  },
 ];
 
 export default CoreValueSection;
