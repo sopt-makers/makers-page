a2110338de5a37b953fbc402a37ad54e4494adf3
Tekiter <tekiter8@gmail.com>
2023-07-27 04:23:45 +0900

    feat: 서비스 바인딩 반영

--- apps/web/src/app/trpc/[[...slug]]/route.ts
+++ apps/web/src/app/trpc/[[...slug]]/route.ts
@@ -1,10 +1,11 @@
 import 'server-only';
 
 import { SSR_GATEWAY_DOMAIN } from '@/env/server';
+import { serviceBindingFetch } from '@/gateway/serviceBinding';
 
 export const runtime = 'edge';
 
-export async function GET(request: Request) {
+async function trpcProxy(request: Request) {
   const url = new URL(request.url);
   const targetUrl = new URL(SSR_GATEWAY_DOMAIN ?? '');
   url.protocol = targetUrl.protocol;
@@ -16,20 +17,8 @@ export async function GET(request: Request) {
     headers: request.headers,
   });
 
-  return await fetch(url, newRequest);
+  return await serviceBindingFetch(url, newRequest);
 }
 
-export async function POST(request: Request) {
-  const url = new URL(request.url);
-  const targetUrl = new URL(SSR_GATEWAY_DOMAIN ?? '');
-  url.protocol = targetUrl.protocol;
-  url.host = targetUrl.host;
-
-  const newRequest = new Request(url, {
-    body: request.body,
-    method: request.method,
-    headers: request.headers,
-  });
-
-  return await fetch(url, newRequest);
-}
+export const GET = trpcProxy;
+export const POST = trpcProxy;
