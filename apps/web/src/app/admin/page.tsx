3ae90c97edb46553217f1bec87a3acb16adcd876
Tekiter <tekiter8@gmail.com>
2023-07-27 04:21:38 +0900

    feat: 어드민 페이지 반영

--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -10,12 +10,13 @@ interface AdminPageProps {}
 const AdminPage: FC<AdminPageProps> = ({}) => {
   const gateway = useGateway();
 
-  const { mutate, status } = useMutation({ mutationFn: () => gateway.recruit.invalidate.mutate() });
+  const { mutate, status, data } = useMutation({ mutationFn: () => gateway.recruit.refresh.mutate() });
 
   return (
-    <div>
-      <button onClick={() => mutate()}>리크루트 페이지 다시 로드</button>
+    <div className='text-18-regular'>
+      <button onClick={() => mutate()}>리크루트 페이지 새로고침</button>
       <p>Result: {status}</p>
+      <p>{JSON.stringify(data)}</p>
     </div>
   );
 };
