74799d8eb6eeda52951f2467a61cf3178b7e2f10
Brokyeom <goindol40@gmail.com>
2023-07-28 00:52:15 +0900

    feat : 마감시간 설정, 라우팅

--- apps/web/src/components/common/RecruitButton.tsx
+++ apps/web/src/components/common/RecruitButton.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import dayjs from 'dayjs';
 import React, { useEffect, useState } from 'react';
 
@@ -5,8 +7,8 @@ export default function RecruitButton() {
   const [timeLeft, setTimeLeft] = useState<string>('');
 
   useEffect(() => {
-    const deadline = dayjs('2023-08-05T13:00:00');
-    const start = dayjs('2023-07-29T00:00:00');
+    const deadline = dayjs('2023-08-05T13:00:00'); // August 5, 2023, 13:00:00
+    const start = dayjs('2023-07-29T00:00:00'); // July 29, 2023, 00:00:00
 
     const calculateTimeLeft = () => {
       const now = dayjs();
@@ -15,20 +17,20 @@ export default function RecruitButton() {
         return '7/29부터 지원 가능';
       }
 
+      if (now.isAfter(deadline)) {
+        return '지원이 마감되었어요.';
+      }
+
       const difference = deadline.diff(now, 'second');
 
-      let timeLeft = '3기 지원이 마감되었어요.';
+      const days = Math.floor(difference / (3600 * 24));
+      const hours = Math.floor((difference / 3600) % 24);
+      const minutes = Math.floor((difference / 60) % 60);
+      const seconds = difference % 60;
 
-      if (difference > 0) {
-        const hours = Math.floor(difference / 3600);
-        const minutes = Math.floor((difference / 60) % 60);
-        const seconds = difference % 60;
-
-        timeLeft = `${hours.toString().padStart(3, ' ')}:${minutes.toString().padStart(2, '0')}:${seconds
-          .toString()
-          .padStart(2, '0')} 뒤 지원 마감`;
-      }
-      return timeLeft;
+      return `3기 지원 마감까지 ${days}일 ${hours.toString().padStart(2, '0')}:${minutes
+        .toString()
+        .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
     };
 
     setTimeLeft(calculateTimeLeft());
@@ -37,11 +39,11 @@ export default function RecruitButton() {
       setTimeLeft(calculateTimeLeft());
     }, 1000);
 
-    return () => clearInterval(timer);
+    return () => clearInterval(timer); // clear the interval on component unmount
   }, []);
 
   return (
-    <button className='bg-dark1 mb-[5.104rem] mt-[4rem] rounded-[1.2rem] border border-solid border-white px-[4rem] py-[1.6rem]'>
+    <button className='bg-dark1 mb-[5.104rem] mt-[4rem] rounded-[1.2rem] border border-solid border-[#808388] px-[4rem] py-[1.6rem]'>
       <p className='text-24-semibold'>3기 합류하기 (~8/05)</p>
       <p className='text-16-regular mt-[1.2rem]'>{timeLeft}</p>
     </button>
