8594f2b9d54086427b847d77131edcaba534b63a
Brokyeom <goindol40@gmail.com>
2023-07-28 04:48:57 +0900

    feat : RecruitSection 모바일 대응

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,7 +6,7 @@ import CoreValueSection from '@/components/mainpage/coreValue/CoreValueSection';
 import FeedbackSection from '@/components/mainpage/feedback/FeedbackSection';
 import GreetingSection from '@/components/mainpage/greeting/GreetingSection';
 import ProductSection from '@/components/mainpage/prouduct/ProductSection';
-import Recruit from '@/components/mainpage/Recruit';
+import RecruitSection from '@/components/mainpage/recruit/RecruitSection';
 
 export default function Page() {
   return (
@@ -17,7 +17,7 @@ export default function Page() {
         <ProductSection />
         <CoreValueSection />
         <FeedbackSection />
-        <Recruit />
+        <RecruitSection />
       </div>
     </ReactLenis>
   );
