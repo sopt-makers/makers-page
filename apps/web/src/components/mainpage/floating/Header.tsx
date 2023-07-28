b3c0bdeaf70c32e3a2ccdaa87ec36e71d8cd55d3
Brokyeom <goindol40@gmail.com>
2023-07-28 22:55:07 +0900

    chore: 미완성 메뉴, 링크 주석처리

--- apps/web/src/components/mainpage/floating/Header.tsx
+++ apps/web/src/components/mainpage/floating/Header.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 
 import MakersLogo from '@/components/common/icons/MakersLogo';
 
@@ -11,9 +11,11 @@ interface NavButton {
 
 export default function Header() {
   const navButtons: NavButton[] = [
+    { name: 'INTRODUTION', position: 3000 },
     { name: 'OUR MISSION', position: 6500 },
     { name: 'SERVICE', position: 7900 },
     { name: 'CORE VALUE', position: 10010 },
+    { name: 'FEEDBACK', position: 14000 },
     // { name: 'MEMBERS', position: undefined },
     { name: 'RECRUIT', position: 50000 },
   ];
@@ -22,14 +24,14 @@ export default function Header() {
     <header className='fixed z-20 flex w-full justify-between py-[4rem] pl-[6rem] pr-[3.808rem]'>
       <section className='flex'>
         <MakersLogo className='mr-[8rem] cursor-pointer' onClick={() => handleScroll(0)} />
-        <div className='text-18-regular flex items-center justify-center gap-[4rem]'>
+        {/* <div className='text-18-regular flex items-center justify-center gap-[4rem]'>
           <Link href='/blog'>
             <span className='hover:text-18-semibold cursor-pointer'>BLOG</span>
           </Link>
           <Link href='/recruit/'>
             <span className='hover:text-18-semibold cursor-pointer'>RECRUIT</span>
           </Link>
-        </div>
+        </div> */}
       </section>
       <nav className='text-16-regular fixed right-0 top-0 flex flex-col items-end gap-[0.608rem] pb-[6.1rem] pl-[3.9rem] pr-[5.4rem] pt-[4.9rem]'>
         {navButtons.map((button, index) => (
