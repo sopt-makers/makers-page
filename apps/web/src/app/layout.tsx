dcbf955765ba9e58381c1db799419583594b1ca3
Tekiter <tekiter8@gmail.com>
2023-07-29 03:40:06 +0900

    feat: GTM 추가

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import './global.css';
 
 import clsx from 'clsx';
 import localFont from 'next/font/local';
+import Script from 'next/script';
 import { ReactNode } from 'react';
 
 import AnimationProviders from '@/components/core/animation/AnimationProviders';
@@ -55,7 +56,20 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='ko' className={clsx(suitFont.variable, 'bg-black100 text-[62.5%] text-white')}>
+      <Script id='init-gtm'>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P7FTW7RG');`}</Script>
       <body className='min-h-screen'>
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-P7FTW7RG'
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <GatewayProvider>
           <AnimationProviders>{children}</AnimationProviders>
         </GatewayProvider>
