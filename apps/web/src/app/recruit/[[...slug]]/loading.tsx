73ea4182e8c24becbc5e3f3974bfa8550336ff0b
Tekiter <tekiter8@gmail.com>
2023-07-28 16:43:51 +0900

    feat: 스켈레톤 랜덤화

--- apps/web/src/app/recruit/[[...slug]]/loading.tsx
+++ apps/web/src/app/recruit/[[...slug]]/loading.tsx
@@ -10,17 +10,12 @@ const RecruitLoading: FC<RecruitLoadingProps> = ({}) => {
         skeletons.space(),
         skeletons.text(12),
         skeletons.text(12),
-        skeletons.text(9),
+        skeletons.text(8),
         skeletons.text(4),
         skeletons.space(),
-        skeletons.text(3),
-        skeletons.text(12),
-        skeletons.text(12),
-        skeletons.text(10),
-        skeletons.text(8),
-        skeletons.text(12),
-        skeletons.text(12),
-        skeletons.text(3),
+        ...randGen(4, 10, () => skeletons.text()),
+        skeletons.space(),
+        ...randGen(5, 8, () => skeletons.text()),
       ]}
     </div>
   );
@@ -28,16 +23,29 @@ const RecruitLoading: FC<RecruitLoadingProps> = ({}) => {
 
 export default RecruitLoading;
 
+function randInt(start: number, end: number) {
+  return Math.floor(Math.random() * (end - start)) + start;
+}
+
+function randGen<T>(start: number, end: number, fn: () => T) {
+  return [...new Array(randInt(start, end))].map(() => fn());
+}
+
 const skeletons = {
   header: (len: number) => (
     <div className='mb-[1rem] grid grid-cols-12'>
       <div className='h-[4rem] rounded-full bg-white/20' style={{ gridColumn: `span ${len} / span ${len}` }} />
     </div>
   ),
-  text: (len: number) => (
-    <div className='grid grid-cols-12'>
-      <div className='h-[1.6rem] rounded-full bg-white/20' style={{ gridColumn: `span ${len} / span ${len}` }} />
-    </div>
-  ),
+  text: (width?: number) => {
+    const f = (x: number) => -1 / 20 / (x + 0.08) + 1;
+    const len = width ?? Math.ceil(f(Math.random()) * 12);
+
+    return (
+      <div className='grid grid-cols-12'>
+        <div className='h-[1.6rem] rounded-full bg-white/20' style={{ gridColumn: `span ${len} / span ${len}` }} />
+      </div>
+    );
+  },
   space: () => <div className='h-[2rem]'></div>,
 };
