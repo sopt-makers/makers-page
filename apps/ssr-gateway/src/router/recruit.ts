f9a573ec3acb060d1c2543024b456126ec4b19d6
Tekiter <tekiter8@gmail.com>
2023-07-29 23:56:42 +0900

    feat: 리크루팅 페이지 로딩 성능 향상 (#63)

--- apps/ssr-gateway/src/router/recruit.ts
+++ apps/ssr-gateway/src/router/recruit.ts
@@ -2,17 +2,22 @@ import { Block } from 'notion-types';
 import { parsePageId } from 'notion-utils';
 import { z } from 'zod';
 
+import { storageFactory } from '../storage/kv';
 import { Context } from '../trpc/context';
 import { internalProcedure, publicProcedure, router } from '../trpc/stub';
 
 export const recruitRouter = router({
   page: internalProcedure.input(z.object({ id: z.string().optional() })).query(async ({ ctx, input }) => {
+    const pageStorage = getPageStorage(ctx.kv);
+    const allowedPagesStorage = getAllowedPagesStorage(ctx.kv);
+    const signedPageStorage = getSignedPageStorage(ctx.kv);
+
     const pageId = parsePageId(input.id ?? ctx.recruit.rootPageId);
     if (!pageId) {
       return { status: 'NOT_FOUND' } as const;
     }
 
-    const allowedPages = (await ctx.kv.get(kvKeys.allowedPages(), 'json')) as string[] | null;
+    const allowedPages = await allowedPagesStorage.get('');
     if (!allowedPages) {
       return { status: 'NEED_REFRESH' } as const;
     }
@@ -21,14 +26,30 @@ export const recruitRouter = router({
       return { status: 'NOT_FOUND' } as const;
     }
 
-    const validated = pageRecordValidator.safeParse(await ctx.kv.get(kvKeys.page(pageId), 'json'));
-    if (!validated.success) {
+    const cached = await signedPageStorage.get(pageId);
+    if (cached) {
+      console.log('cached');
+      return { status: 'SUCCESS', ...cached } as const;
+    }
+
+    const data = await pageStorage.get(pageId);
+
+    if (!data) {
       return { status: 'NEED_REFRESH' } as const;
     }
 
-    const { blockMap, ...pageData } = validated.data;
+    const { blockMap, ...pageData } = data;
 
-    const blockMapSigned = await ctx.recruit.notionClient.SignFileUrls(blockMap);
+    const blockMapSigned = await (async () => {
+      return await ctx.recruit.notionClient.SignFileUrls(blockMap);
+    })();
+
+    const newSignedPage = {
+      ...pageData,
+      blockMap: blockMapSigned,
+    };
+
+    await signedPageStorage.put(pageId, newSignedPage, { expirationTtl: 60 * 50 });
 
     return { status: 'SUCCESS', ...pageData, blockMap: blockMapSigned } as const;
   }),
@@ -45,21 +66,36 @@ type PathFragment = {
   title: string;
 };
 
-const pageRecordValidator = z.object({
-  version: z.literal(1),
+const pageSchema = z.object({
   id: z.string(),
   title: z.string().nullable(),
   path: z.array(z.custom<PathFragment>()),
   blockMap: z.custom<Record<string, Block>>(),
 });
-type PageRecord = z.infer<typeof pageRecordValidator>;
 
-const kvKeys = {
-  page: (pageId: string) => `recruit:page:${pageId}`,
-  allowedPages: () => 'recruit:allowdPages',
-};
+const getPageStorage = storageFactory({
+  version: 2,
+  prefix: 'recruit:page:',
+  type: pageSchema,
+});
+
+const getSignedPageStorage = storageFactory({
+  version: 2,
+  prefix: 'recruit:signedPage:',
+  type: pageSchema,
+});
+
+const getAllowedPagesStorage = storageFactory({
+  version: 1,
+  prefix: 'recruit:allowedPages',
+  type: z.array(z.string()),
+});
 
 async function refetchPages(ctx: Context) {
+  const pageStorage = getPageStorage(ctx.kv);
+  const allowedPagesStorage = getAllowedPagesStorage(ctx.kv);
+  const signedPagesStorage = getSignedPageStorage(ctx.kv);
+
   const rootPageId = parsePageId(ctx.recruit.rootPageId);
   const allowedPages: string[] = [];
 
@@ -89,18 +125,17 @@ async function refetchPages(ctx: Context) {
     const childTraversePromises = childPageIds.map(async (id) => traverse(id, newPath));
     await Promise.all(childTraversePromises);
 
-    const record: PageRecord = {
-      version: 1,
+    await pageStorage.put(pageId, {
       id: pageId,
       title,
       path: newPath,
       blockMap,
-    };
-    await ctx.kv.put(kvKeys.page(pageId), JSON.stringify(record));
+    });
 
     allowedPages.push(pageId);
   }
 
   await traverse(rootPageId);
-  await ctx.kv.put(kvKeys.allowedPages(), JSON.stringify(allowedPages));
+  await allowedPagesStorage.put('', allowedPages);
+  await signedPagesStorage.deleteAll();
 }
