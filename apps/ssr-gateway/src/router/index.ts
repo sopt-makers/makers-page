08e660fab5a38994ab1b97d5ecd5b6a2485cc110
Tekiter <tekiter8@gmail.com>
2023-07-29 13:42:28 +0900

    feat: URL 리다이렉트 추가

--- apps/ssr-gateway/src/router/index.ts
+++ apps/ssr-gateway/src/router/index.ts
@@ -1,10 +1,12 @@
 import { router } from '../trpc/stub';
 import { blogRouter } from './blog';
 import { recruitRouter } from './recruit';
+import { redirectRouter } from './redirect';
 
 export const appRouter = router({
   recruit: recruitRouter,
   blog: blogRouter,
+  redirect: redirectRouter,
 });
 
 // export type definition of API
