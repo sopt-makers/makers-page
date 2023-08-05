cc1de74f830603028116ba15b222de8cb25d2980
Tekiter <tekiter8@gmail.com>
2023-08-05 15:49:27 +0900

    feat: 표기 수정

--- apps/web/src/components/common/RecruitButton.tsx
+++ apps/web/src/components/common/RecruitButton.tsx
@@ -48,7 +48,7 @@ export default function RecruitButton() {
       href='/recruit/'
       className='bg-dark1 mb-[3rem] mt-[4rem] rounded-[1.2rem] border border-solid border-[#808388] px-[4rem] py-[1.6rem]'
     >
-      <p className='md:text-24-semibold text-18-semibold'>3기 합류하기 (~8/5)</p>
+      <p className='md:text-24-semibold text-18-semibold'>3기 합류하기 (~8/7)</p>
       <p className='text-16-regular mt-[1.2rem] text-center'>{timeLeft}</p>
     </Link>
   );
