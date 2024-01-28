5448b3cc140612e27c9f4f7755bebf0cd89fbc54
Brokyeom <goindol40@gmail.com>
2024-01-28 23:18:08 +0900

    chore : 플레이그라운드 이미지 변경

--- apps/web/src/components/mainpage/prouduct/data.ts
+++ apps/web/src/components/mainpage/prouduct/data.ts
@@ -11,7 +11,7 @@ export const descriptionMap: DescriptionMap = {
     altText: 'official_image',
   },
   playground: {
-    imageUrl: 'https://github.com/sopt-makers/makers-page/assets/97586683/fc756699-5c1f-4c1c-98d0-dd50d2c910cf',
+    imageUrl: 'https://github.com/sopt-makers/makers-page/assets/97586683/d86f0d9a-34cc-4e01-ad72-930e1211843c',
     altText: 'playground_image',
   },
   app: {
