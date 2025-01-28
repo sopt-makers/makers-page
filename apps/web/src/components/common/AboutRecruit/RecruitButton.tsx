200030cec951faffad28716764e2200e374f4905
Brokyeom <goindol40@gmail.com>
2025-01-29 00:56:49 +0900

    feat: Update recruit button for 36th recruitment cycle

--- apps/web/src/components/common/AboutRecruit/RecruitButton.tsx
+++ apps/web/src/components/common/AboutRecruit/RecruitButton.tsx
@@ -10,14 +10,14 @@ export default function RecruitButton() {
   const [timeLeft, setTimeLeft] = useState<string>('');
 
   useEffect(() => {
-    const deadline = dayjs('2024-08-09T14:59:00.000Z');
-    const start = dayjs('2024-07-31T02:00:00.000Z');
+    const deadline = dayjs('2025-02-05T14:59:00.000Z');
+    const start = dayjs('2025-01-29T02:00:00.000Z');
 
     const calculateTimeLeft = () => {
       const now = dayjs();
 
       if (now.isBefore(start)) {
-        return '7/31 11:00 부터 지원 가능';
+        return '1/29 11:00 부터 지원 가능';
       }
 
       if (now.isAfter(deadline)) {
@@ -32,8 +32,8 @@ export default function RecruitButton() {
       // const seconds = difference % 60;
 
       if (days > 0 || hours >= 24) {
-        // return '현재 35기 메이커스 팀 모집 중 (~8/9 금요일 23시 59분)';
-        return '일부 포지션 모집 연장 (~8/9 금요일 23시 59분)';
+        return '현재 36기 메이커스 팀 모집 중 (~2/5 수요일 23시 59분)';
+        // return '일부 포지션 모집 연장 (~2/6 목요일 14시 59분)';
       }
 
       return `지원 마감까지 ${hours.toString()}시간 ${minutes.toString().padStart(2, '0')}분 남았어요.`;
@@ -50,12 +50,11 @@ export default function RecruitButton() {
 
   return (
     <>
-      <Link
-        href='/recruit/'
-        className='mb-[0.5rem] mt-[4rem] rounded-[1.2rem] bg-white'
-      >
-        {/* <Button theme='white' size='lg' RightIcon={IconChevronRight}>35기 지원하기</Button> */}
-        <Button theme='white' size='lg' RightIcon={IconChevronRight}>35기 모집글 보기</Button>
+      <Link href='/recruit/' className='mb-[0.5rem] mt-[4rem] rounded-[1.2rem] bg-white'>
+        <Button theme='white' size='lg' RightIcon={IconChevronRight}>
+          36기 지원하기
+        </Button>
+        {/* <Button theme='white' size='lg' RightIcon={IconChevronRight}>35기 모집글 보기</Button> */}
       </Link>
       <p className='text-16-regular text-brand-orange mb-[4.2rem] mt-[1.2rem] text-center'>{timeLeft}</p>
     </>
